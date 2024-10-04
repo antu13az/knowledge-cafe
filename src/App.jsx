764daed8d433fd11b47b446f71bb7d13cdde1e53
antu13az <sajalhowlader08.com>
2024-10-05 00:21:16 +0600

    add new blogs

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import "./App.css";
+import Blogs from "./Component/Blogs/Blogs";
+import Header from "./Component/Header/Header";
 
 function App() {
-  
-
-  return <>
-  <h2 className="text-3xl font-bold text-center text-red-500 uppercase">Vite React Project for Tailwind mane lej oala css</h2>
-  </>;
+  return (
+    <>
+      <h2 className="text-3xl font-bold text-center text-red-500 uppercase">
+        Vite React Project for Tailwind mane lejoala css
+      </h2>
+      <Header />
+      <Blogs />
+    </>
+  );
 }
 
 export default App;
