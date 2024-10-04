4590a6d2fc110c720df2c9165168541467cc6ffc
antu13az <sajalhowlader08.com>
2024-10-04 18:36:11 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  
+
+  return <>
+  <h2 className="text-3xl font-bold text-center text-red-500 uppercase">Vite React Project for Tailwind mane lej oala css</h2>
+  </>;
+}
+
+export default App;
